bcc4b8db34b4e813658b7e80c1332bf240a24705
Oscar <oscar.delrio95@hotmail.com>
2020-08-25 19:44:28 -0400

    first commit

---
+++ src/components/result.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <section class="game__result">
+      <div class="game__you">
+        <p>Elegiste {{yourSelected}}</p>
+        <div class="game__paper" v-bind:class="{ 'game--winner': final == 'Has Ganado' }"></div>
+      </div>
+      <div class="game__final">
+        <p class="game__title">{{final}}</p>
+        <button class="btn btn__primary" @click="again()">Jugar de nuevo</button>
+      </div>
+      <div class="game__rival">
+        <p>Tu rival eligió {{rivalSelected}}</p>
+        <div class="game__scissor" v-bind:class="{ 'game--winner': final == 'Has Perdido' }"></div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Result",
+  methods: {
+    again() {
+      this.$emit("gameAgain", false);
+    },
+  },
+  props: {
+    winner: String,
+    rivalSelected: String,
+    yourSelected: String,
+    final: String,
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,114 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="indicators">
+      <div class="indicators__types">
+        <h2 class="indicators__type">Piedra</h2>
+        <h2 class="indicators__type">Papel</h2>
+        <h2 class="indicators__type">Tijera</h2>
+      </div>
+      <div class="indicators__scores">
+        <span class="indicators__title">Puntaje</span>
+        <h1 class="indicators__score">{{score}}</h1>
+      </div>
+    </section>
+
+    <result
+      v-if="isResult"
+      v-on:gameAgain="gameAgain($event)"
+      :rivalSelected="enemySelected"
+      :yourSelected="typeSelected"
+      :final="winner"
+    ></result>
+    <template v-if="!isResult">
+      <section class="game__selected" v-if="typeSelected">
+        <h1 class="game__title">Has elegido {{typeSelected}}</h1>
+      </section>
+
+      <section class="game__selected" v-if="!typeSelected">
+        <h1 class="game__title">Selecciona Piedra, Papel y Tijera</h1>
+      </section>
+
+      <section class="game" v-bind:class="{'game--process': isProcessing}">
+        <div @click="onSelected('Papel')" class="game__paper"></div>
+        <div @click="onSelected('Tijera')" class="game__scissor"></div>
+        <div @click="onSelected('Piedra')" class="game__rock"></div>
+      </section>
+    </template>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Result from "@/components/result";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Result,
+  },
+  data() {
+    return {
+      isResult: false,
+      isProcessing: false,
+      winner: "",
+      typeSelected: "",
+      score: 0,
+      max: 2,
+      min: 0,
+      enemySelected: "",
+      possibilities: ["Papel", "Tijera", "Piedra"],
+      isLoading: false
+    };
+  },
+  methods: {
+    onSelected(name) {
+      this.typeSelected = name;
+      this.isProcessing = true;
+
+      if (!this.isLoading) {
+        this.isLoading = true;
+        setTimeout(() => {
+          this.isProcessing = false;
+          this.isResult = true;
+
+          const enemy = Math.floor(
+            Math.random() * (this.max - this.min + 1) + this.min
+          );
+          this.enemySelected = this.possibilities[enemy];
+          this.validate();
+        }, 2000);
+      }
+    },
+    gameAgain(again) {
+      this.isResult = again;
+      this.typeSelected = "";
+    },
+    validate() {
+      if (this.enemySelected == this.typeSelected) {
+        this.winner = "Empate";
+      } else {
+        if (this.enemySelected == "Papel" && this.typeSelected == "Tijera") {
+          this.winner = "Has Ganado";
+          this.score++;
+        } else if (
+          this.enemySelected == "Tijera" &&
+          this.typeSelected == "Piedra"
+        ) {
+          this.winner = "Has Ganado";
+          this.score++;
+        } else if (
+          this.enemySelected == "Papel" &&
+          this.typeSelected == "Tijera"
+        ) {
+          this.winner = "Has Ganado";
+          this.score++;
+        } else {
+          this.winner = "Has Perdido";
+          if (this.score > 0) {
+            this.score--;
+          }
+        }
+      }
+      this.isLoading = false;
+    },
+  },
+};
 </script>
